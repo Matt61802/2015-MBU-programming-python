005cfde4a5abb477143ad79b50d509282362a2f7
Matthew Cheffro <mattcheffro@gmail.com>
2015-04-04 15:59:42 +0000

    Automatic commit

--- mbu_assignment.py
+++ mbu_assignment.py
@@ -35,7 +35,13 @@ def main():
     #This is for loop. We take each element from sys.argv and print it to the consile.
     for i in sys.argv[1:]:
         print str(i)
+        if int(i) % 2==0: 
+            print "even"
+        else: 
+            print "odd"
         print "\n"
-
+    
+        
+        
 if __name__ == '__main__':
     main()
